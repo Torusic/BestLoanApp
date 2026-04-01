99cd917ef303b2e15c100b19d50732278e4e4375
Torusic <smuchuku7@gmail.com>
2026-04-01 09:58:10 +0300

    'update'

--- client/src/DashBoard/pages/actions/AddCustomer.jsx
+++ client/src/DashBoard/pages/actions/AddCustomer.jsx
@@ -8,7 +8,7 @@ function AddCustomer() {
           <p className='text-xs font-semibold lg:text-sm my-2'>Add Tenant</p>
 
           <div className=' grid'>
-            <div className='grid grid-cols-2 lg:grid-cols-3 text-xs'>
+            <div className='grid grid-cols-2 lg:grid-cols-3 items-center justify-between gap-2 text-xs'>
                 <div className='grid items-center justify-between gap-2 my-2'>
                     <label htmlFor="">Name</label>
                     <input type="text" className='outline-none bg-gray-100 p-2 w-full rounded-lg' placeholder='Enter customers name...'/>
