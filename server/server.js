b4aca25c1dd11e4a8f0fa24056d8eef3053be683
Torusic <smuchuku7@gmail.com>
2026-04-01 19:56:54 +0300

    all

--- server/server.js
+++ server/server.js
@@ -14,6 +14,22 @@ dotenv.config()
 const app=express()
 
 app.use(express.json())
+const allowedOrigins = [
+  "http://localhost:5173",
+  process.env.FRONTEND_URL
+]
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true)
+    } else {
+      callback(new Error("Not allowed by CORS"))
+    }
+  },
+  credentials: true
+}))
+
 app.use(cookieParser())
 app.use(
   cors({
