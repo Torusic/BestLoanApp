93d3ad055bd3189ac505fb10175dbc5cabed702c
Torusic <smuchuku7@gmail.com>
2026-04-01 09:56:26 +0300

    'add'

--- client/src/DashBoard/pages/Customers.jsx
+++ client/src/DashBoard/pages/Customers.jsx
@@ -1,22 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IoAdd } from 'react-icons/io5'
+import AddCustomer from './actions/AddCustomer'
 
 function Customers() {
+  const[customer, setCustomer]=useState(false)
   return (
-    <section className='bg-gray-200'>
-      <div className='bg-white p-2 max-w-md w-full '>
+    <section className='bg-gray-200 '>
+      <div className='bg-white p-2 max-w-md w-full lg:max-w-7xl lg:w-full '>
         <h2 className='text-lg font-bold text-gray-500 my-2'>Customers</h2>
         <div className='flex items-center  gap-1'>
-           <span className='bg-gray-100 p-2 rounded-lg cursor-pointer'><IoAdd /></span>
+           <span className='bg-gray-100 p-2 rounded-lg cursor-pointer'onClick={()=>setCustomer(true)}><IoAdd /></span>
           <p className='text-xs'>Add Customer</p>
          
 
         </div>
 
       </div>
+      {
+      customer && (
+        <AddCustomer
+        onClick={()=>setCustomer(false)}
+        />
+      )
+    }
 
 
     </section>
+    
   )
 }
 
---
+++ client/src/DashBoard/pages/actions/AddCustomer.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+function AddCustomer() {
+  return (
+    
+    <section className='bg-gray-950/50 fixed top-0 flex  items-center justify-center bottom-0 right-0 left-0 '>
+         <div className='bg-white py-6 px-5 max-w-sm md:max-w-md w-full lg:max-w-3xl lg:w-full rounded mx-2 '>
+          <p className='text-xs font-semibold lg:text-sm my-2'>Add Tenant</p>
+
+          <div className=' grid'>
+            <div className='grid grid-cols-2 lg:grid-cols-3 text-xs'>
+                <div className='grid items-center justify-between gap-2 my-2'>
+                    <label htmlFor="">Name</label>
+                    <input type="text" className='outline-none bg-gray-100 p-2 w-full rounded-lg' placeholder='Enter customers name...'/>
+                </div>
+                <div className='grid items-center justify-between gap-2 my-2'>
+                    <label htmlFor="">Phone</label>
+                    <input type="text" className='outline-none bg-gray-100 p-2 w-full rounded-lg' placeholder='Enter customers phone...'/>
+                </div>
+                <div className='grid items-center justify-between gap-2 my-2'>
+                    <label htmlFor="">ID Number</label>
+                    <input type="text" className='outline-none bg-gray-100 p-2 w-full rounded-lg' placeholder='Enter customers national id...'/>
+                </div>
+                <div className='grid items-center justify-between gap-2 my-2'>
+                    <label htmlFor="">Amount</label>
+                    <input type="text" className='outline-none bg-gray-100 p-2 w-full rounded-lg' placeholder='Enter customers  amount...'/>
+                </div>
+                <div className='grid items-center justify-between gap-2 my-2 w-full'>
+                    <label htmlFor="">Duration</label>
+                    <input type="text" className='outline-none bg-gray-100 p-2 rounded-lg w-full' placeholder='Enter customers duration...'/>
+                </div>
+                 <div className='flex items-center justify-center my-3 mt-6'>
+                    <button className=' w-full cursor-pointer text-white font-semibold bg-green-400 p-3 rounded-lg '>
+                        Add Customer
+                    </button>
+
+                </div>
+                
+
+            </div>
+
+          </div>
+        </div>
+
+    </section>
+  )
+}
+
+export default AddCustomer
