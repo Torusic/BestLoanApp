7d5dacdf844dcf96c572354e8b83b6fd69abc6c6
Torusic <smuchuku7@gmail.com>
2026-04-06 21:12:49 +0300

    'uu'

--- client/src/DashBoard/pages/MyLoan.jsx
+++ client/src/DashBoard/pages/MyLoan.jsx
@@ -126,7 +126,13 @@ function MyLoan() {
                    
                </div>
 
-                <div className=' gri'>
+                <div className=' grid'>
+                    
+               {!active.isFeePaid && (
+                <button onClick={()=>setFee(true)} className="bg-blue-500 text-sm text-white px-4 py-2  rounded">
+                     Pay Processing Fee: ({formatCurrency(active.processingFee)})
+                </button>
+                )}
 
                 <div className="bg-gray-900 text-white p-2 rounded text-xs lg:text-sm md:text-sm grid gap-2">
                     
