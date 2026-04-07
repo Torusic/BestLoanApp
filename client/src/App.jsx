f798673e5eff047494dc098abd7033c299354b7e
Torusic <smuchuku7@gmail.com>
2026-04-07 13:06:43 +0300

    'k'

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <>
     
-    <main className='min-h-[90vh] bg-gray-900'>
+    <main className='min-h-[100vh] bg-gray-900 '>
       <Outlet/>
     </main>
  <Toaster/>
